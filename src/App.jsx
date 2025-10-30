679ee06fa5ece03b3ee8ffdf4b2235c47c8f681d
silcaclei <silviacachonleiva@gmail.com>
2025-10-30 18:22:22 +0100

    Arreglo 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Interprete from "./Interprete.jsx";
 function App() {
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <h1 className="font-heading-h1 text-[var(--color-primary)] text-center">
+      <h1 className="heading-h1 text-[var(--color-primary)] text-center">
         Intérpretes
       </h1>
       <p className="text-medium text-[var(--color-grey-3)] text-center">
@@ -12,6 +12,48 @@ function App() {
       </p>
 
         <Contenedor>
+          <Interprete
+            nombre="Marlon Brando"
+            foto="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Marlon_Brando_publicity_for_One-Eyed_Jacks.png/270px-Marlon_Brando_publicity_for_One-Eyed_Jacks.png"
+          >
+            Marlon Brando fue un influyente actor estadounidense...
+          </Interprete>
+
+          <Interprete
+            nombre="Al Pacino"
+            foto="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Al_Pacino.jpg/220px-Al_Pacino.jpg"
+          >
+            Al Pacino es un actor y director de cine estadounidense...
+          </Interprete>
+
+          <Interprete
+            nombre="Marlon Brando"
+            foto="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Marlon_Brando_publicity_for_One-Eyed_Jacks.png/270px-Marlon_Brando_publicity_for_One-Eyed_Jacks.png"
+          >
+            Marlon Brando fue un influyente actor estadounidense...
+          </Interprete>
+
+          <Interprete
+            nombre="Al Pacino"
+            foto="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Al_Pacino.jpg/220px-Al_Pacino.jpg"
+          >
+            Al Pacino es un actor y director de cine estadounidense...
+          </Interprete>
+
+          <Interprete
+            nombre="Marlon Brando"
+            foto="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Marlon_Brando_publicity_for_One-Eyed_Jacks.png/270px-Marlon_Brando_publicity_for_One-Eyed_Jacks.png"
+          >
+            Marlon Brando fue un influyente actor estadounidense...
+          </Interprete>
+
+          <Interprete
+            nombre="Al Pacino"
+            foto="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Al_Pacino.jpg/220px-Al_Pacino.jpg"
+          >
+            Al Pacino es un actor y director de cine estadounidense...
+          </Interprete>
+
           <Interprete
             nombre="Marlon Brando"
             foto="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Marlon_Brando_publicity_for_One-Eyed_Jacks.png/270px-Marlon_Brando_publicity_for_One-Eyed_Jacks.png"
--- src/Interprete.jsx
+++ src/Interprete.jsx
@@ -1,9 +1,11 @@
 function Interprete({ nombre, foto, children }) {
   return (
-    <div className="p-4 shadow rounded-lg bg-[var(--color-white)]">
-      <h2 className="font-heading-h2 text-[var(--color-primary)] mb-2">{nombre}</h2>
-      <img src={foto} alt={nombre} className="w-full rounded mb-2" />
-      <p className="text-medium text-[var(--color-grey-2)]">{children}</p>
+    <div className="card flex flex-col">
+      <img src={foto} alt={nombre} className="w-full rounded mb-3" />
+      <div className="flex-1">
+        <h2 className="heading-h2 text-[var(--color-secondary)] mb-1">{nombre}</h2>
+        <p className="text-medium text-[var(--color-grey-2)]">{children}</p>
+      </div>
     </div>
   );
 }
