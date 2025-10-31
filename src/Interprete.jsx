68edb41961cea7c4f93e64299c56218483526e25
silcaclei <silviacachonleiva@gmail.com>
2025-10-31 17:06:38 +0100

    Arrelgo 3: Añadiendo accesibilidad

--- src/Interprete.jsx
+++ src/Interprete.jsx
@@ -1,12 +1,17 @@
 function Interprete({ nombre, foto, children }) {
   return (
-    <div className="card flex flex-col">
-      <img src={foto} alt={nombre} className="w-full rounded mb-3" />
-      <div className="flex-1">
+    <article className="card flex flex-col">
+      <figure className="mb-3">
+        <img src={foto} alt={nombre} className="w-full rounded" />
+        <figcaption className="text-center mt-2">
+          {nombre}
+        </figcaption>
+      </figure>
+      <main className="flex-1">
         <h2 className="heading-h2 text-[var(--color-secondary)] mb-1">{nombre}</h2>
         <p className="text-medium text-[var(--color-grey-2)]">{children}</p>
-      </div>
-    </div>
+      </main>
+    </article>
   );
 }
 
--- src/InterpretePeli10.jsx
+++ src/InterpretePeli10.jsx
@@ -1,12 +1,17 @@
 function Interprete({ nombre, foto, esNota10, children }) {
   return (
-    <div className="card flex flex-col">
-      <img src={foto} alt={nombre} className="w-full rounded mb-3 " />
-      <div className="flex-1">
-        <h2 className={`"heading-h2 text-[var(--color-secondary)] mb-1" ${esNota10 ? 'text-red-500' : ''}`}>{nombre}</h2>
+    <article className="card flex flex-col">
+      <figure className="mb-3">
+        <img src={foto} alt={nombre} className="w-full rounded" />
+        <figcaption className="text-center mt-2">
+          {nombre}
+        </figcaption>
+      </figure>
+      <main className="flex-1">
+        <h2 className={`heading-h2 mb-1 ${esNota10 ? 'text-red-500' : 'text-[var(--color-secondary)]'}`}>{nombre}</h2>
         <p className="text-medium text-[var(--color-grey-2)]">{children}</p>
-      </div>
-    </div>
+      </main>
+    </article>
   );
 }
 
